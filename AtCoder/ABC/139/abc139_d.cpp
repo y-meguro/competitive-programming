62ff8a0f93bab1e0340a7751c6157b6ec4cc4c30
Yohei Meguro <y.meguro.2008@gmail.com>
2020-01-30 18:01:11 +0900

    add abc139_d.cpp

---
+++ AtCoder/ABC/139/abc139_d.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  ll N;
+  cin >> N;
+
+  cout << N * (N - 1) / 2 << endl;
+
+  return 0;
+}
