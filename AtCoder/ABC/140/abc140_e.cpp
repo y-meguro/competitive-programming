b8702278271faea6a3828bc0b883a200859bbd94
Yohei Meguro <y.meguro.2008@gmail.com>
2020-02-06 16:53:40 +0900

    update abc140_e

--- AtCoder/ABC/140/abc140_e.cpp
+++ AtCoder/ABC/140/abc140_e.cpp
@@ -24,14 +24,10 @@ int main() {
     // インデックスの順は l1 < l2 < pos.at(i) < r1 < r2 となるようにする
     ms.insert(pos.at(i));
     auto itr = ms.find(pos.at(i));
-    rep(j, 2) itr--;
-    ll l1 = *itr;
-    itr++;
-    ll l2 = *itr;
-    rep(j, 2) itr++;
-    ll r1 = *itr;
-    itr++;
-    ll r2 = *itr;
+    ll l1 = *prev(itr, 2);
+    ll l2 = *prev(itr, 1);
+    ll r1 = *next(itr, 1);
+    ll r2 = *next(itr, 2);
 
     ans += ((l2 - l1) * (r1 - pos.at(i)) + (pos.at(i) - l2) * (r2 - r1)) * i;
   }
