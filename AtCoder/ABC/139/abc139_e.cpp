5c386b316e6c53ce2aef15f1b978c1f6d235e7bc
Yohei Meguro <y.meguro.2008@gmail.com>
2020-02-07 11:27:21 +0900

    add abc139_e

--- AtCoder/ABC/139/abc139_e.cpp
+++ AtCoder/ABC/139/abc139_e.cpp
@@ -3,39 +3,43 @@
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
-const int MAXN = 1001;
-const int MAXV = MAXN * (MAXN - 1) / 2;
 
-vector<vector<int>> id(MAXN, vector<int>(MAXN));
+vector<vector<int>> A;
+vector<vector<int>> id;
+
 int toId(int i, int j) {
-  if (i > j) swap(i, j);
-  return id.at(i).at(j);
+  int a = i;
+  int b = A.at(i).at(j); // 対戦相手
+  if (a > b) swap(a, b);
+  return id.at(a).at(b);
 }
 
-vector<vector<int>> to(MAXV);
-vector<int> dp(MAXV);
-vector<bool> reached(MAXV);
-vector<bool> calculated(MAXV);
+vector<vector<int>> G;
+vector<int> dp;
+vector<bool> reached;
+vector<bool> completed;
+
 int dfs(int v) {
   if (reached.at(v)) {
-    if (!calculated.at(v)) return -1;
+    if (!completed.at(v)) return -1;
     return dp.at(v);
   }
   reached.at(v) = true;
   dp.at(v) = 1;
-  for (int u : to.at(v)) {
+  for (int u : G.at(v)) {
     int res = dfs(u);
     if (res == -1) return -1;
     dp.at(v) = max(dp.at(v), res + 1);
   }
-  calculated.at(v) = true;
+  completed.at(v) = true;
   return dp.at(v);
 }
 
 int main() {
   int N;
   cin >> N;
-  vector<vector<int>> A(N, vector<int>(N - 1));
+
+  A.resize(N, vector<int>(N - 1));
   rep(i, N) {
     rep(j, N - 1) {
       cin >> A.at(i).at(j);
@@ -43,33 +47,36 @@ int main() {
     }
   }
 
+  // 試合用のidを作成、参加者を行と列に持つので、N * N の大きさが必要
+  id.resize(N, vector<int>(N));
   int V = 0;
   rep(i, N) {
     rep(j, N) {
-      if (i < j) {
-        id.at(i).at(j) = V++;
-      }
+      if (i >= j) continue;
+      id.at(i).at(j) = V++;
     }
   }
 
+  int NUMV = N * (N - 1) / 2;
+  G.resize(NUMV);
+  dp.resize(NUMV);
+  reached.resize(NUMV);
+  completed.resize(NUMV);
+
   rep(i, N) {
-    rep(j, N - 1) {
-      A.at(i).at(j) = toId(i, A.at(i).at(j));
-    }
     rep(j, N - 2) {
-      to.at(A.at(i).at(j + 1)).push_back(A.at(i).at(j)); // 逆向きに辺をはる
+      G.at(toId(i, j + 1)).push_back(toId(i, j)); // 辺を逆順に張っておく
     }
   }
 
   int ans = 0;
-  rep(i, V) {
+  rep(i, NUMV) {
     int res = dfs(i);
-    if (res == -1){
+    if (res == -1) {
       cout << -1 << endl;
       return 0;
-    } else {
-      ans = max(ans, res);
     }
+    ans = max(ans, res);
   }
   cout << ans << endl;
   return 0;
