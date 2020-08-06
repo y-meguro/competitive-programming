6d634bf9fa08390c4537b0d0f3d939ed841129b7
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-07 00:27:01 +0900

    update scripts

--- AtCoder/cpsubmit_atcoder.sh
+++ AtCoder/cpsubmit_atcoder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 oj s "https://atcoder.jp/contests/${1:0:6}/tasks/${1:0:8}" $1
 # oj s "https://${1:0:6}.contest.atcoder.jp/tasks/${1:0:8}" $1
-# oj s "https://atcoder.jp/contests/dp/tasks/dp_f" $1
+# oj s "https://atcoder.jp/contests/dp/tasks/${1:0:4}" $1
--- AtCoder/cptest_atcoder.sh
+++ AtCoder/cptest_atcoder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# oj dl "https://atcoder.jp/contests/${1:0:6}/tasks/${1:0:8}"
-# oj dl "https://${1:0:6}.contest.atcoder.jp/tasks/${1:0:8}"
-oj dl "https://atcoder.jp/contests/dp/tasks/${1:0:4}"
+oj d "https://atcoder.jp/contests/${1:0:6}/tasks/${1:0:8}"
+# oj d "https://${1:0:6}.contest.atcoder.jp/tasks/${1:0:8}"
+# oj d "https://atcoder.jp/contests/dp/tasks/${1:0:4}"
 g++ -Wall -std=c++14 ./$1
-oj test
+oj t
 rm -f a.out
 rm -rf test
