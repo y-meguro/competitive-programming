8d5b312232cf2fa595391d1cc3c413794422cb3c
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-19 16:36:16 +0900

    remove unnecessary file

--- AtCoder/ABC/167/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-
-  //   cout << "i " << i << " C.at(i) " << C.at(i) << endl;
-  //   rep(j, M) {
-  //     cout << "i " << i << " j " << j << " A.at(i).at(j) " << A.at(i).at(j) << endl;
-  //   }
-  // }
