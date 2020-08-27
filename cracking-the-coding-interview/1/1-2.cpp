bc6fdfd4e96ff4b51464a9bdeab954b99c6fac15
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-27 16:40:13 +0900

    update 1-2

--- cracking-the-coding-interview/1/1-2.cpp
+++ cracking-the-coding-interview/1/1-2.cpp
@@ -7,10 +7,24 @@ bool isRearranged(string x, string y) {
   return x == y;
 }
 
+bool isRearranged2(string x, string y) {
+  if (x.size() != y.size()) return false;
+
+  vector<int> chars(26, 0);
+  for (int i = 0; i < (int)x.size(); i++) {
+    chars.at(x.at(i) - 'a')++;
+  }
+  for (int i = 0; i < (int)y.size(); i++) {
+    chars.at(y.at(i) - 'a')--;
+    if (chars.at(y.at(i) - 'a') < 0) return false;
+  }
+  return true;
+}
+
 int main() {
   string x, y;
   cin >> x >> y;
-  string ans = isRearranged(x, y) ? "true" : "false";
+  string ans = isRearranged2(x, y) ? "true" : "false";
   cout << ans << endl;
   return 0;
 }
