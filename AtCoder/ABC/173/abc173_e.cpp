27c7f9585e5876c16d778c23365352f94a811ddc
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-28 20:33:13 +0900

    update abc173_e

--- AtCoder/ABC/173/abc173_e.cpp
+++ AtCoder/ABC/173/abc173_e.cpp
@@ -8,7 +8,7 @@ ll mod = 1e9 + 7;
 int main() {
   int n, k;
   cin >> n >> k;
-  vector<int> a(n);
+  vector<int> a(n, 0);
   vector<int> pos, neg;
   rep(i, n) {
     cin >> a.at(i);
@@ -16,24 +16,26 @@ int main() {
     else pos.push_back(a.at(i));
   }
 
+  // 正にできる組み合わせがあるか調べる
+  bool ok = true;
   int posSize = pos.size();
   int negSize = neg.size();
-  bool ok = false; // 選び方によって、答えを正にすることができる場合 true
-  if (posSize > 0) {
-    if (n == k) ok = (negSize % 2 == 0);
-    else ok = true;
-  } else {
+  if (posSize == 0) {
     ok = (k % 2 == 0);
+  } else {
+    if (n == k) ok = (negSize % 2 == 0);
   }
 
   ll ans = 1;
   if (!ok) {
+    // できない場合は絶対値の小さい順に取る
     sort(a.begin(), a.end(), [](int x, int y) {
       return abs(x) < abs(y);
     });
     rep(i, k) ans = ans * a.at(i) % mod;
     ans = mod + ans;
   } else {
+    // できる場合は積を大きくできる組み合わせを選ぶ
     sort(pos.begin(), pos.end());
     sort(neg.rbegin(), neg.rend());
     if (k % 2 == 1) {
@@ -42,15 +44,15 @@ int main() {
     }
 
     vector<ll> p;
-    while (pos.size() >= 2) {
-      ll x = pos.back(); pos.pop_back();
-      x *= pos.back(); pos.pop_back();
-      p.push_back(x);
+    while (pos.size() > 1) {
+      ll tmp = pos.back(); pos.pop_back();
+      tmp *= pos.back(); pos.pop_back();
+      p.push_back(tmp);
     }
-    while (neg.size() >= 2) {
-      ll x = neg.back(); neg.pop_back();
-      x *= neg.back(); neg.pop_back();
-      p.push_back(x);
+    while (neg.size() > 1) {
+      ll tmp = neg.back(); neg.pop_back();
+      tmp *= neg.back(); neg.pop_back();
+      p.push_back(tmp);
     }
     sort(p.rbegin(), p.rend());
     rep(i, k/2) ans = ans * (p.at(i) % mod) % mod;
