27495779414cbf1973e412de7bc02eb4edb0594a
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-29 12:03:45 +0900

    add abc163_d

---
+++ AtCoder/ABC/163/abc163_d.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+ll mod = 1e9 + 7;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+ll sum(ll l, ll r) {
+  return (l + r) * (r - l + 1) / 2;
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  mint ans = 0;
+  for (int i = k; i <= n + 1; i++) {
+    ll l = sum(0, i - 1), r = sum(n - i + 1, n);
+    ans += r - l + 1;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
