1b71375c4d17ca0580a05c54fb45d606ead1792d
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-31 00:49:38 +0900

    update 2-1

--- cracking-the-coding-interview/2/2-1.cpp
+++ cracking-the-coding-interview/2/2-1.cpp
@@ -6,6 +6,27 @@ struct Node {
   Node* next = nullptr;
 };
 
+// 先頭に挿入する
+void insert(Node*& head, int data) { // ポインタ変数が参照される。参照されるので呼び出し元のheadも書き換わる
+  Node* newNode = new Node;
+  newNode->data = data;
+  newNode->next = head; // nextには1つ前に先頭だったnodeのポインタが入る
+  head = newNode;
+}
+
+void printList(Node* head) {
+  while (head) {
+    cout << head->data << " --> ";
+    head = head->next;
+  }
+  cout << "nullptr" << endl;
+}
+
 int main() {
+  Node* head = nullptr;
+  for (int i = 0; i < 10; i++) {
+    insert(head, i);
+  }
+  printList(head);
   return 0;
 }
