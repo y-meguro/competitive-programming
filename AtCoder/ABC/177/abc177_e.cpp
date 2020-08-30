1294f2dc2bef7d60651c1167966e03839a92df2f
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-30 15:24:31 +0900

    add abc177_e

---
+++ AtCoder/ABC/177/abc177_e.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+const int A = 1e6 + 1;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  vector<int> c(A);
+  rep(i, n) {
+    cin >> a.at(i);
+    c.at(a.at(i))++;
+  }
+
+  bool pairwise = true;
+  for (int i = 2; i < A; i++) {
+    int cnt = 0;
+    for (int j = i; j < A; j += i) {
+      cnt += c.at(j);
+    }
+    if (cnt > 1) pairwise = false;
+  }
+  if (pairwise) {
+    cout << "pairwise coprime" << endl;
+    return 0;
+  }
+
+  int g = 0;
+  rep(i, n) g = __gcd(g, a.at(i));
+  if (g == 1) {
+    cout << "setwise coprime" << endl;
+    return 0;
+  }
+
+  cout << "not coprime" << endl;
+  return 0;
+}
