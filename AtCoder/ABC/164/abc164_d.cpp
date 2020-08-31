41bfd8faf9085dd14a4de3a282f92ca5a396b32f
Yohei Meguro <y.meguro.2008@gmail.com>
2020-08-31 20:58:19 +0900

    update abc164_d

--- AtCoder/ABC/164/abc164_d.cpp
+++ AtCoder/ABC/164/abc164_d.cpp
@@ -11,10 +11,10 @@ int main() {
   reverse(s.begin(), s.end());
 
   // 余りをカウントする
+  ll ans = 0;
+  int tot = 0, x = 1;
   vector<int> cnt(m, 0);
   cnt.at(0) = 1;
-  int tot = 0, x = 1;
-  ll ans = 0;
   for (int i = 0; i < (int)s.size(); i++) {
     tot += (s.at(i) - '0') * x;
     tot %= m;
