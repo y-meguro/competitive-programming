04de57a6cafe3b01a5c02894a43eb145453e5918
Yohei Meguro <y.meguro.2008@gmail.com>
2020-09-03 09:06:02 +0900

    update abc177_d

--- AtCoder/ABC/177/abc177_d.cpp
+++ AtCoder/ABC/177/abc177_d.cpp
@@ -4,40 +4,46 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+struct UnionFind {
+  vector<int> d; // 根なら負の値でサイズを持つ。子なら親のIDを持つ
+  UnionFind(int n = 0) : d(n, -1) {}
+
+  int find(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = find(d[x]);
+  }
+
+  bool unite(int x, int y) {
+    x = find(x); y = find(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x, y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+
+  bool same(int x, int y) {
+    return find(x) == find(y);
+  }
+
+  int size(int x) {
+    return -d[find(x)];
+  }
+};
+
 int main() {
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> f(n);
+  UnionFind uf(n);
   rep(i, m) {
     int a, b;
     cin >> a >> b;
-    a--; b--;
-    f.at(a).push_back(b);
-    f.at(b).push_back(a);
+    --a; --b;
+    uf.unite(a, b);
   }
 
-  vector<bool> checked(n, false);
-  // 全部の点からはじまるグループの人数を求める
-  deque<int> q;
   int ans = 0;
-  rep(i, n) {
-    if (checked.at(i)) continue;
-    q.push_back(i);
-    checked.at(i) = true;
-    int tmp = 1;
-    while (!q.empty()) {
-      int x = q.front();
-      q.pop_front();
-      for (auto u : f.at(x)) {
-        if (checked.at(u)) continue;
-        q.push_back(u);
-        checked.at(u) = true;
-        tmp++;
-      }
-    }
-    ans = max(ans, tmp);
-  }
-
+  rep(i, n) ans = max(ans, uf.size(i));
   cout << ans << endl;
   return 0;
 }
