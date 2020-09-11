321d0f6253968dc845fe97b2ea6ab7bea8963752
Yohei Meguro <y.meguro.2008@gmail.com>
2020-09-11 13:00:14 +0900

    add stack

---
+++ cracking-the-coding-interview/3/stack.hpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class Stack {
+  public:
+    Stack() : top(nullptr), stackSize(0) {}
+    Stack(Stack&& other) : top(move(other.top)), stackSize(move(other.stackSize)) {}
+    ~Stack() {
+      while (!isEmpty()) pop();
+    }
+
+    template <typename U>
+    void push(U&& value) {
+      auto n = new Node(forward<U>(value), top);
+      top = n;
+      ++stackSize;
+    }
+
+    T& peek() {
+      if (!top) throw StackIsEmptyException();
+      return top->value;
+    }
+
+    T pop() {
+      if (!top) throw StackIsEmptyException();
+      auto value(move(top->value));
+      auto n = top;
+      top = n->next;
+      delete n;
+      --stackSize;
+      return value;
+    }
+
+    bool isEmpty() const {
+      return !top;
+    }
+
+    size_t size() const {
+      return stackSize;
+    }
+
+    class StackIsEmptyException {};
+
+  private:
+    Node* top;
+    size_t stackSize;
+
+    struct Node {
+      T value;
+      Node* next;
+
+      Node(T&& v, Node* n) : value(move(v)), next(n) {}
+      Node(const T& v, Node* n) : value(v), next(n) {}
+    };
+};
