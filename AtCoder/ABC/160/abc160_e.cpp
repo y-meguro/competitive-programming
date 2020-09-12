d066cd77933d4433c68ec0944dbd3bf854875863
Yohei Meguro <y.meguro.2008@gmail.com>
2020-09-12 23:27:36 +0900

    update abc160_e

--- AtCoder/ABC/160/abc160_e.cpp
+++ AtCoder/ABC/160/abc160_e.cpp
@@ -13,28 +13,15 @@ int main() {
   rep(i, c) cin >> r.at(i);
   sort(p.rbegin(), p.rend());
   sort(q.rbegin(), q.rend());
-  sort(r.rbegin(), r.rend());
+
+  vector<int> s;
+  rep(i, x) s.push_back(p.at(i));
+  rep(i, y) s.push_back(q.at(i));
+  rep(i, c) s.push_back(r.at(i));
+  sort(s.rbegin(), s.rend());
 
   ll ans = 0;
-  vector<int> s;
-  rep(i, x) {
-    s.push_back(p.at(i));
-    ans += p.at(i);
-  }
-  rep(i, y) {
-    s.push_back(q.at(i));
-    ans += q.at(i);
-  }
-  sort(s.begin(), s.end());
-
-  int n = min((int)s.size(), c);
-  for (int i = 0; i < n; i++) {
-    if (s.at(i) < r.at(i)) {
-      ans += r.at(i) - s.at(i);
-    } else {
-      break;
-    }
-  }
+  rep(i, x + y) ans += s.at(i);
   cout << ans << endl;
   return 0;
 }
