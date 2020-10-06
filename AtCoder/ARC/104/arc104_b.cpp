dbe4a51fd4d88e6ccc80ae0c41098f844783bd23
Yohei Meguro <y.meguro.2008@gmail.com>
2020-10-06 22:26:02 +0900

    update arc104_b

--- AtCoder/ARC/104/arc104_b.cpp
+++ AtCoder/ARC/104/arc104_b.cpp
@@ -9,44 +9,26 @@ int main() {
   string s;
   cin >> n >> s;
 
-  vector<int> numA(n, 0), numT(n, 0), numC(n, 0), numG(n, 0);
-  rep(i, n) {
-    if (i != 0) {
-      numA.at(i) = numA.at(i - 1);
-      numT.at(i) = numT.at(i - 1);
-      numC.at(i) = numC.at(i - 1);
-      numG.at(i) = numG.at(i - 1);
-    }
+  vector<int> numA(n + 1, 0), numT(n + 1, 0), numC(n + 1, 0), numG(n + 1, 0);
+  for (int i = 1; i <= n; i++) {
+    numA.at(i) = numA.at(i - 1);
+    numT.at(i) = numT.at(i - 1);
+    numC.at(i) = numC.at(i - 1);
+    numG.at(i) = numG.at(i - 1);
 
-    if (s[i] == 'A') {
-      if (i == 0) numA.at(i) = 1;
-      else numA.at(i)++;
-    } else if (s[i] == 'T') {
-      if (i == 0) numT.at(i) = 1;
-      else numT.at(i)++;
-    } else if (s[i] == 'C') {
-      if (i == 0) numC.at(i) = 1;
-      else numC.at(i)++;
-    } else {
-      if (i == 0) numG.at(i) = 1;
-      else numG.at(i)++;
-    }
+    if (s[i - 1] == 'A') numA.at(i)++;
+    else if (s[i - 1] == 'T') numT.at(i)++;
+    else if (s[i - 1] == 'C') numC.at(i)++;
+    else numG.at(i)++;
   }
 
   int ans = 0;
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) {
-      int a = numA.at(j);
-      int t = numT.at(j);
-      int c = numC.at(j);
-      int g = numG.at(j);
-
-      if (i != 0) {
-        a -= numA.at(i - 1);
-        t -= numT.at(i - 1);
-        c -= numC.at(i - 1);
-        g -= numG.at(i - 1);
-      }
+  for (int i = 0; i <= n - 1; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      int a = numA.at(j) - numA.at(i);
+      int t = numT.at(j) - numT.at(i);
+      int c = numC.at(j) - numC.at(i);
+      int g = numG.at(j) - numG.at(i);
       if (a == t && c == g) ans++;
     }
   }
